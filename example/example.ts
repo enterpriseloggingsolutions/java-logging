55853812ab9ef0ea5eba52be6ab65517ba9b3dad
Alec Strickland <alecjstrickland@gmail.com>
2022-07-13 23:47:13 -0700

    Fix the example file

--- example/example.ts
+++ example/example.ts
@@ -1,4 +1,8 @@
+/* this file doesn't work, but here we are */
 import { System } from 'java-logging'
 
 System.out.println("sup") /* so much more maintinable */
 System.out.print("hello") /* far more clean */
+
+System.err.println('test');
+System.err.print('test');
