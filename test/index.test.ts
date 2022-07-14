becb6200bf5e83affeec2944974cf3cdfa160d5d
enterpriseloggingsolutions <109264349+enterpriseloggingsolutions@users.noreply.github.com>
2022-07-14 01:21:33 -0500

    finally, a better logging solution

---
+++ test/index.test.ts
@@ -0,0 +1,9 @@
+import { System } from '../lib/index'
+
+test('can print', () => {
+  System.out.print('important messages')
+})
+
+test('can println', () => {
+  System.out.println('more important messages')
+})
