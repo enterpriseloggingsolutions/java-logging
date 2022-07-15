3d0d683dc5cf8858db5fee02b12a3d1e33dcbe69
enterpriseloggingsolutions <109264349+enterpriseloggingsolutions@users.noreply.github.com>
2022-07-14 21:31:08 -0500

    finally, a better logging solution

--- lib/Out.ts
+++ lib/Out.ts
@@ -3,7 +3,7 @@ import { PrintStream } from './PrintStream';
 export class Out implements PrintStream {
 
   public print(txt) {
-    process.stdout.write(txt);
+    process.stdout.write(txt + '');
   }
 
   public println(txt) {
--- test/index.test.ts
+++ test/index.test.ts
@@ -21,6 +21,20 @@ test.each([
   System.out.println(value)
 })
 
+test.each([
+  "an enterprise grade string",
+  "another string because two is better than one, can never be too sure these days you know?",
+  1994,
+  23.1789,
+  true,
+  false,
+  null,
+  undefined,
+])('can out.println all types', (value) => {
+  System.out.print(value)
+})
+
+
 test('can err.print', () => {
   System.err.print('important messages')
 })
@@ -40,4 +54,17 @@ test.each([
   undefined,
 ])('can err.println all types', (value) => {
   System.err.println(value)
+})
+
+test.each([
+  "an enterprise grade string",
+  "another string because two is better than one, can never be too sure these days you know?",
+  1994,
+  23.1789,
+  true,
+  false,
+  null,
+  undefined,
+])('can err.println all types', (value) => {
+  System.err.print(value)
 })
